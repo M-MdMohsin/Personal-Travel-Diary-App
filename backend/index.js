9a090e7ca4a9b0da8a1ad69dacae512621320e00
M-MdMohsin <emceeee62GMAIL.COM>
2025-10-03 11:44:43 +0530

    create signip api route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 
@@ -15,6 +16,14 @@ mongoose
 
 const app = express();
 
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }));
+
+
 app.listen(3000, () => {
     console.log("Server is Running on port 3000!")
 })
+
+app.use("/api/auth", authRoutes)
