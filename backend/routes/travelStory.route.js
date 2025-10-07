4fcaeafaac8e70064f4a4850ec5dccca75f70dbb
M-MdMohsin <emceeee62GMAIL.COM>
2025-10-07 17:43:42 +0530

    image delete api route

--- backend/routes/travelStory.route.js
+++ backend/routes/travelStory.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { verifyToken } from "../utils/varifyUser.js"
-import { addTravelStory, getAllTravelStory, imageUpload } from "../controllers/travelStory.controller.js"
+import { addTravelStory, deleteImage, getAllTravelStory, imageUpload } from "../controllers/travelStory.controller.js"
 import upload from "../multer.js"
 
 
@@ -11,5 +11,7 @@ router.post("/add", verifyToken, addTravelStory)
 router.get("/get-all", verifyToken, getAllTravelStory)
 
 router.post("/image-upload", upload.single("image"), imageUpload)
+
+router.delete("/delete-image", deleteImage)
  
 export default router
