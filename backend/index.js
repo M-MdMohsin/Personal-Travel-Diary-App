1a5d8b26c15e50d9158689c5df082e3f721cf89e
M-MdMohsin <emceeee62GMAIL.COM>
2025-10-08 16:10:12 +0530

    edit travel story api route

--- backend/index.js
+++ backend/index.js
@@ -33,6 +33,8 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 app.use("/uploads", express.static(path.join(__dirname,"uploads")))
 
+app.use("/assets", express.static(path.join(__dirname, "assets")))
+
 app.use(express.urlencoded({ extended: true }));
 
 
--- backend/routes/travelStory.route.js
+++ backend/routes/travelStory.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { verifyToken } from "../utils/varifyUser.js"
-import { addTravelStory, deleteImage, getAllTravelStory, imageUpload } from "../controllers/travelStory.controller.js"
+import { addTravelStory, deleteImage, editTravelStory, getAllTravelStory, imageUpload } from "../controllers/travelStory.controller.js"
 import upload from "../multer.js"
 
 
@@ -13,5 +13,7 @@ router.get("/get-all", verifyToken, getAllTravelStory)
 router.post("/image-upload", upload.single("image"), imageUpload)
 
 router.delete("/delete-image", deleteImage)
+
+router.post("/edit-story/:id", verifyToken, editTravelStory)
  
 export default router
