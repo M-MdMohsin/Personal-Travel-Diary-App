7554da73ec78aa881fbb9489d5559dd8955a0dae
M-MdMohsin <emceeee62GMAIL.COM>
2025-10-24 16:54:25 +0530

    get all story integration

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar'
+import axiosInstance from '../../utils/axiosInstance'
 
 const Home = () => {
+
+  const [allStories, setAllStories] = useState([])
+
+  console.log(allStories)
+
+  const getAllTravelStories = async () => {
+    try {
+      const response = await axiosInstance.get("/travel-story/get-all")
+
+      if (response.data && response.data.stories) {
+        setAllStories(response.data.stories)
+      }
+    } catch (error) {
+      console.log("Something went wrong. Please try again.")
+    }
+  }
+
+  useEffect(() => {
+    getAllTravelStories()
+
+    return () => {}
+  }, [])
+
   return (
     <>
       <Navbar />
